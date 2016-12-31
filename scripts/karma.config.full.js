d655e1a43e6218d6a74ce15eda421503b41e315c
Dolphin Wood <dolphin.w.e@gmail.com>
2016-12-31 11:39:18 +0900

    add(test): add full browsers tests

---
+++ scripts/karma.config.full.js
@@ -0,0 +1,9 @@
+const baseConfig = require('./karma.config.base');
+
+module.exports = function(config) {
+  config.set(Object.assign(baseConfig, {
+    browsers: ['Chrome', 'Firefox', 'Safari'],
+    singleRun: true,
+    autoWatch: false,
+  }));
+}
