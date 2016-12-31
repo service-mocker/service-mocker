fb7fda9b12202a9a4e23bcb0ae2ee88658bdcb49
Dolphin Wood <dolphin.w.e@gmail.com>
2016-12-31 22:41:16 +0900

    add(test): add coverage report

---
+++ scripts/karma.config.cover.js
@@ -0,0 +1,28 @@
+const path = require('path');
+const baseConfig = require('./karma.config.base');
+
+module.exports = function(config) {
+  baseConfig.webpack.module.postLoaders = [
+      // instrument only testing sources with Istanbul
+      {
+          test: /\.ts$/,
+          loader: 'sourcemap-istanbul-instrumenter?force-sourcemap=true',
+          include: path.join(__dirname, '..', 'src'),
+      }
+  ];
+
+  config.set(Object.assign(baseConfig, {
+    browsers: ['Chrome'],
+    reporters: ['mocha', 'karma-remap-istanbul'],
+    preprocessors: {
+      '**/*.ts': ['webpack', 'sourcemap'],
+    },
+
+    remapIstanbulReporter: {
+      reports: {
+        'text-summary': null,
+        html: './coverage/html',
+      },
+    },
+  }));
+}
