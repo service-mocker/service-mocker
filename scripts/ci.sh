483cad704e8dae143e0f65646150c0256527c1f7
Dolphin Wood <dolphin.w.e@gmail.com>
2017-01-03 08:33:30 +0900

    update(ci): run all tests on PR

--- scripts/ci.sh
+++ scripts/ci.sh
@@ -2,16 +2,17 @@
 set -e
 
 # no need for `npm run test`
-# all tests are executed in cover task
+# all tests are executed in cover/sauce task
 npm run lint
-npm run cover
+npm run test:sauce
 
 # report coverage except PRs
 if [[ -z $CI_PULL_REQUEST ]]; then
+  npm run cover
   ./node_modules/.bin/codecov
 fi
 
 # run sauce for develop/master
-if [ $CIRCLE_BRANCH == "develop" -o $CIRCLE_BRANCH == "master" ]; then
-  npm run test:sauce
-fi
+# if [ $CIRCLE_BRANCH == "develop" -o $CIRCLE_BRANCH == "master" ]; then
+#   npm run test:sauce
+# fi
