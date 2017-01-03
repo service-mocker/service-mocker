fa1dc3329517a93d2ca57b50ef6add1c017f2c22
Dolphin Wood <dolphin.w.e@gmail.com>
2017-01-03 09:11:24 +0900

    update(ci): report coverage on every PR

--- scripts/ci.sh
+++ scripts/ci.sh
@@ -5,12 +5,16 @@ set -e
 # all tests are executed in cover/sauce task
 npm run lint
 npm run test:sauce
+npm run cover
+./node_modules/.bin/codecov
+
+
 
 # report coverage except PRs
-if [[ -z $CI_PULL_REQUEST ]]; then
-  npm run cover
-  ./node_modules/.bin/codecov
-fi
+# if [[ -z $CI_PULL_REQUEST ]]; then
+#   npm run cover
+#   ./node_modules/.bin/codecov
+# fi
 
 # run sauce for develop/master
 # if [ $CIRCLE_BRANCH == "develop" -o $CIRCLE_BRANCH == "master" ]; then
