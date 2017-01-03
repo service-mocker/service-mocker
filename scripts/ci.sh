da1659897d5be6b642aa640a28ff81453ddcd916
Dolphin Wood <dolphin.w.e@gmail.com>
2017-01-04 05:13:25 +0900

    update(ci): temporarily disable sauce

--- scripts/ci.sh
+++ scripts/ci.sh
@@ -4,7 +4,7 @@ set -e
 # no need for `npm run test`
 # all tests are executed in cover/sauce task
 npm run lint
-npm run test:sauce
+# npm run test:sauce
 npm run cover
 ./node_modules/.bin/codecov
 
