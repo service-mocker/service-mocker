b77ecf6fe26ba06bfee0fcd46557d41e22cba408
VincentBel <buaazqh@gmail.com>
2017-01-04 10:31:37 +0800

    chore(ci): enable sauce test

--- scripts/ci.sh
+++ scripts/ci.sh
@@ -4,19 +4,6 @@ set -e
 # no need for `npm run test`
 # all tests are executed in cover/sauce task
 npm run lint
-# npm run test:sauce
+npm run test:sauce
 npm run cover
 ./node_modules/.bin/codecov
-
-
-
-# report coverage except PRs
-# if [[ -z $CI_PULL_REQUEST ]]; then
-#   npm run cover
-#   ./node_modules/.bin/codecov
-# fi
-
-# run sauce for develop/master
-# if [ $CIRCLE_BRANCH == "develop" -o $CIRCLE_BRANCH == "master" ]; then
-#   npm run test:sauce
-# fi
