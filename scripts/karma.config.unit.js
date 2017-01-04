480850869bae59348ccbd9276714863bd79aca50
Dolphin Wood <dolphin.w.e@gmail.com>
2017-01-05 01:09:23 +0900

    chore: replace nyan with our version

--- scripts/karma.config.unit.js
+++ scripts/karma.config.unit.js
@@ -2,6 +2,7 @@ const baseConfig = require('./karma.config.base');
 
 module.exports = function (config) {
   config.set(Object.assign(baseConfig, {
+    reporters: ['nyan'],
     browsers: ['Chrome'],
   }));
 };
