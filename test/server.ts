dff9a1f7498ad434bc174059035cb11b59d63799
Dolphin Wood <dolphin.w.e@gmail.com>
2017-01-10 02:44:15 +0900

    fix(test/server): init legacy server at runtime

--- test/server.ts
+++ test/server.ts
@@ -3,21 +3,21 @@ import { RESPONSE } from './spec/mock-response';
 import { createServer } from 'service-mocker/server';
 import { serverRunner } from './tools/server-runner';
 
-const server = createServer();
-
-const mode = server.isLegacy ? 'Legacy' : 'Modern';
-
-// mocks
-server.router.get('/api', RESPONSE);
-server.router.get('/blob', new Blob());
-server.router.get('/json', { res: RESPONSE });
-server.router.get('/document', `<p>${RESPONSE}</p>`);
-server.router.get('/arraybuffer', new ArrayBuffer(100));
-server.router.get('/custom-header', (req, res) => {
-  res.send(req.headers.get('X-Custom'));
-});
-
 serverRunner(() => {
+  const server = createServer();
+
+  const mode = server.isLegacy ? 'Legacy' : 'Modern';
+
+  // mocks
+  server.router.get('/api', RESPONSE);
+  server.router.get('/blob', new Blob());
+  server.router.get('/json', { res: RESPONSE });
+  server.router.get('/document', `<p>${RESPONSE}</p>`);
+  server.router.get('/arraybuffer', new ArrayBuffer(100));
+  server.router.get('/custom-header', (req, res) => {
+    res.send(req.headers.get('X-Custom'));
+  });
+
   describe(`${mode} Server Tests`, function() {
     Object.keys(tests).forEach(name => {
       tests[name].call(this);
--- test/spec/client/legacy/base.ts
+++ test/spec/client/legacy/base.ts
@@ -2,13 +2,12 @@ import { expect } from 'chai';
 import { createClient } from 'service-mocker/client';
 
 export function baseRunner() {
-  const client = createClient('server.js', {
-    forceLegacy: true,
-  });
-
   describe('Infrastructure', () => {
     describe('.isLegacy', () => {
       it('should have a `.isLegacy` property', () => {
+        // legacy client should be initialized on runtime
+        // avoid affecting modern server
+        const client = createLegacyClient();
         expect(client).to.have.property('isLegacy')
           .and.that.is.true;
       });
@@ -16,11 +15,13 @@ export function baseRunner() {
 
     describe('.ready', () => {
       it('should have a `.ready` property', () => {
+        const client = createLegacyClient();
         expect(client).to.have.property('ready')
           .and.that.is.instanceof(Promise);
       });
 
       it('should resolved with `null`', async () => {
+        const client = createLegacyClient();
         const reg = await client.ready;
 
         expect(reg).to.be.null;
@@ -29,16 +30,19 @@ export function baseRunner() {
 
     describe('.controller', () => {
       it('should have a `.controller` property', () => {
+        const client = createLegacyClient();
         expect(client).to.have.property('controller');
       });
 
       it('should be `null`', () => {
+        const client = createLegacyClient();
         expect(client.controller).to.be.null;
       });
     });
 
     describe('.getRegistration()', () => {
       it('should resolved with `null`', async () => {
+        const client = createLegacyClient();
         const reg = await client.getRegistration();
         expect(reg).to.be.null;
       });
@@ -46,10 +50,17 @@ export function baseRunner() {
 
     describe('.update()', () => {
       it('should resolved with `null`', async () => {
+        const client = createLegacyClient();
         const latestReg = await client.update();
 
         expect(latestReg).to.be.null;
       });
     });
+  });
+}
+
+function createLegacyClient() {
+  return createClient('server.js', {
+    forceLegacy: true,
   });
 }
