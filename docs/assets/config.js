b9068928b456e8571647b8cd13e0dc39b3617a16
Dolphin Wood <dolphin.w.e@gmail.com>
2017-01-10 17:32:01 +0900

    fix(docs/landing): fix twitter widget reloading

--- docs/assets/config.js
+++ docs/assets/config.js
@@ -4,17 +4,24 @@ self.$config = {
   plugins: [
     landingScriptsPlugin([
       'https://unpkg.com/webfontloader@latest',
+      'assets/twitter-widget.js',
       'assets/stroke.js',
     ]),
   ],
 };
 
 function landingScriptsPlugin(scripts) {
+  var caches = {};
+
   return function loadScripts(context) {
     context.router.afterEach(function (to) {
       if (to.meta && to.meta.name === 'landing') {
         scripts.forEach(function (url) {
-          var el = document.createElement('script');
+          if (caches[url]) {
+            caches[url].parentElement.removeChild(caches[url]);
+          }
+
+          var el = caches[url] = document.createElement('script');
           el.src = url; el.async = false;
           document.body.appendChild(el);
         });
