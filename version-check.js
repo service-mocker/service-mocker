bd77ec2fa386cf1cb52c04b1b383fc2009dc9a2d
Dolphin Wood <dolphin.w.e@gmail.com>
2017-04-07 15:19:11 +0900

    chore(version-check): update output format

--- version-check.js
+++ version-check.js
@@ -111,7 +111,7 @@ try {
   installDeps('semver')
     .then(compareVer)
     .catch((error) => {
-      console.error(`\x1b[31m${error.message}\x1b[0m`);
+      console.error(`\x1b[31m${error}\x1b[0m`);
       process.exit(1);
     });
 } catch (e) {}
