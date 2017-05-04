e0ad98885856f1ee754f1df41c5123574e63ee95
VincentBel <buaazqh@gmail.com>
2017-05-04 10:33:45 +0800

    chore(lint): improve eslint config
    Use `.js` format, because
    
    1. `.eslintrc` is deprecated
    2. We are more familiar with `js` or `json`, but we can't add comments
       in json file
    
    See more at:
    <http://eslint.org/docs/user-guide/configuring#configuration-file-formats>

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,4 @@
-{
+module.exports = {
   "extends": "standard",
   "parser": "babel-eslint",
   "env": {
---
+++ test/.eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  "env": {
+    "mocha": true
+  },
+  "globals": {
+    // Set each global variable name equal to true to
+    // allow the variable to be overwritten or false to
+    // disallow overwriting
+    // <http://eslint.org/docs/user-guide/configuring#specifying-globals>
+    "Mocha": false
+  },
+  "rules": {
+    // We will see lots of unused-exporessions when using chai
+    // like: expect(err).not.to.be.null
+    "no-unused-expressions": 0
+  }
+}
